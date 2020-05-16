32a6763f1cea5ab2e6113bfe172ca5de737af273
Patrick Post <www.drummer_456@hotmail.com>
2020-05-16 10:08:12 +0200

    First start of the project. Able to load the account.csv of DeGiro and read it as a table. Will use this table to generate a dividend table.

---
+++ Ribbon1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace ExcelBelegger
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ribbon1));
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group3 = this.Factory.CreateRibbonGroup();
+            this.button13 = this.Factory.CreateRibbonButton();
+            this.button14 = this.Factory.CreateRibbonButton();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.button2 = this.Factory.CreateRibbonButton();
+            this.button4 = this.Factory.CreateRibbonButton();
+            this.button8 = this.Factory.CreateRibbonButton();
+            this.button9 = this.Factory.CreateRibbonButton();
+            this.group2 = this.Factory.CreateRibbonGroup();
+            this.button3 = this.Factory.CreateRibbonButton();
+            this.button5 = this.Factory.CreateRibbonButton();
+            this.button6 = this.Factory.CreateRibbonButton();
+            this.button7 = this.Factory.CreateRibbonButton();
+            this.button10 = this.Factory.CreateRibbonButton();
+            this.button11 = this.Factory.CreateRibbonButton();
+            this.button12 = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group3.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.group2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group3);
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Groups.Add(this.group2);
+            this.tab1.Label = "Excel Belegger";
+            this.tab1.Name = "tab1";
+            // 
+            // group3
+            // 
+            this.group3.Items.Add(this.button13);
+            this.group3.Items.Add(this.button14);
+            this.group3.Label = "DeGiro data";
+            this.group3.Name = "group3";
+            // 
+            // button13
+            // 
+            this.button13.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button13.Image = ((System.Drawing.Image)(resources.GetObject("button13.Image")));
+            this.button13.Label = "Open CSV";
+            this.button13.Name = "button13";
+            this.button13.ShowImage = true;
+            this.button13.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.loadAccountData);
+            // 
+            // button14
+            // 
+            this.button14.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button14.Image = ((System.Drawing.Image)(resources.GetObject("button14.Image")));
+            this.button14.Label = "Dividend tabel";
+            this.button14.Name = "button14";
+            this.button14.ShowImage = true;
+            this.button14.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.createDividendPivotTable);
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.button1);
+            this.group1.Items.Add(this.button2);
+            this.group1.Items.Add(this.button4);
+            this.group1.Items.Add(this.button8);
+            this.group1.Items.Add(this.button9);
+            this.group1.Label = "Ratio";
+            this.group1.Name = "group1";
+            // 
+            // button1
+            // 
+            this.button1.Label = "P/B ratio";
+            this.button1.Name = "button1";
+            // 
+            // button2
+            // 
+            this.button2.Label = "P/E ratio";
+            this.button2.Name = "button2";
+            // 
+            // button4
+            // 
+            this.button4.Label = "Debt to Equity ratio";
+            this.button4.Name = "button4";
+            // 
+            // button8
+            // 
+            this.button8.Label = "PEG";
+            this.button8.Name = "button8";
+            // 
+            // button9
+            // 
+            this.button9.Label = "Dividend payout ratio";
+            this.button9.Name = "button9";
+            // 
+            // group2
+            // 
+            this.group2.Items.Add(this.button3);
+            this.group2.Items.Add(this.button5);
+            this.group2.Items.Add(this.button6);
+            this.group2.Items.Add(this.button7);
+            this.group2.Items.Add(this.button10);
+            this.group2.Items.Add(this.button11);
+            this.group2.Items.Add(this.button12);
+            this.group2.Label = "Junk";
+            this.group2.Name = "group2";
+            // 
+            // button3
+            // 
+            this.button3.Label = "EPS";
+            this.button3.Name = "button3";
+            // 
+            // button5
+            // 
+            this.button5.Label = "ROA";
+            this.button5.Name = "button5";
+            // 
+            // button6
+            // 
+            this.button6.Label = "ROE";
+            this.button6.Name = "button6";
+            // 
+            // button7
+            // 
+            this.button7.Label = "ROI";
+            this.button7.Name = "button7";
+            // 
+            // button10
+            // 
+            this.button10.Label = "Dividend yield";
+            this.button10.Name = "button10";
+            // 
+            // button11
+            // 
+            this.button11.Label = "EBIT";
+            this.button11.Name = "button11";
+            // 
+            // button12
+            // 
+            this.button12.Label = "EBITDA";
+            this.button12.Name = "button12";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group3.ResumeLayout(false);
+            this.group3.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.group2.ResumeLayout(false);
+            this.group2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button4;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button5;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button6;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button7;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button8;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button9;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button10;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button11;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button12;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button13;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button14;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ Ribbon1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Tools.Ribbon;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelBelegger
+{
+    public partial class Ribbon1
+    {
+        private int rowIndex;
+        private int columnIndex; // column A
+
+
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        public void FormatAsTable(Excel.Range SourceRange, string TableName, string TableStyleName)
+        {
+            // Check bouwen of tabel al bestaat
+            
+            SourceRange.Worksheet.ListObjects.Add(Excel.XlListObjectSourceType.xlSrcRange,
+            SourceRange, System.Type.Missing, Excel.XlYesNoGuess.xlYes, System.Type.Missing).Name =
+                TableName;
+            SourceRange.Select();
+            SourceRange.Worksheet.ListObjects[TableName].TableStyle = TableStyleName;
+        }
+
+        private void createDividendPivotTable(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Worksheet xlSheet = Globals.ThisAddIn.Application.ActiveWorkbook.ActiveSheet;
+
+            Excel.Range currentFind = null;
+            Excel.Range firstFind = null;
+
+            if(!xlSheet.Name.Equals("Account"))
+            {
+                MessageBox.Show("Selecteer het 'Account' tabblad.");
+                return;
+            }
+            char c = 'P';
+
+            Excel.Range accountTable = xlSheet.ListObjects["Account"].Range;
+
+            Excel.Range dividendTable;
+
+
+            currentFind = accountTable.Find("dividend", Missing.Value,
+           Excel.XlFindLookIn.xlValues, Excel.XlLookAt.xlPart,
+           Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlNext, false, Missing.Value, Missing.Value);
+
+            int i =1;
+
+            while (currentFind != null)
+            {
+                // Keep track of the first range you find. 
+                if (firstFind == null)
+                {
+                    firstFind = currentFind;
+                }
+
+                // If you didn't move to a new range, you are done.
+                else if (currentFind.get_Address(Excel.XlReferenceStyle.xlA1)
+                      == firstFind.get_Address(Excel.XlReferenceStyle.xlA1))
+                {
+                    break;
+                }
+
+               dividendTable  = xlSheet.get_Range(c.ToString() + i);
+
+
+                MessageBox.Show("Cell: " + currentFind.Column + " : " + currentFind.Row);
+               
+                
+                currentFind.Rows.Font.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Red);
+                currentFind.Rows.Font.Bold = true;
+
+                dividendTable.Value2 = currentFind.Value2;
+
+                currentFind = accountTable.FindNext(currentFind);
+
+                i++;
+            }
+
+
+            
+        }
+
+
+        private void loadAccountData(object sender, RibbonControlEventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                filePath = openFileDialog.FileName;
+
+                //Read the contents of the file into a stream
+                var fileStream = openFileDialog.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    string currentLine;
+                    // currentLine will be null when the StreamReader reaches the end of file
+
+                    
+
+
+
+                    Excel.Worksheet xlSheet = Globals.ThisAddIn.Application.ActiveWorkbook.ActiveSheet;
+                    xlSheet.Name = "Account";
+
+
+                    rowIndex = 1;
+
+                    while ((currentLine = reader.ReadLine()) != null)
+                    {
+                        
+
+
+                        String[] seperated = Regex.Split(currentLine, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");//currentLine.Split(',');
+
+
+                        if (currentLine.Contains("geldmarktfonds"))
+                            continue;
+
+
+                        columnIndex = 65; // column A
+
+                        foreach (String s in seperated)
+                        {
+                            char c = (char)columnIndex;
+
+                            Excel.Range test = xlSheet.get_Range(c.ToString() + rowIndex);
+                            test.Value2 = s;
+                            columnIndex++;
+                        }
+
+                        rowIndex++;
+
+                        fileContent += currentLine + "/n";
+                        
+                        
+                    }
+
+                    MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+
+                    char check = (char) (columnIndex-1);
+                    Excel.Range SourceRange = (Excel.Range)xlSheet.get_Range("A1", check.ToString() + (rowIndex-1)); // or whatever range you want here
+                    FormatAsTable(SourceRange, "Account", "TableStyleLight9");
+                }
+            }
+        }
+    }
+}
